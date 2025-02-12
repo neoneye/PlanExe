c6481f4466dc75af36265b25903c7530ec8f4c4b
Simon Strandgaard <neoneye@gmail.com>
2025-02-12 03:23:25 +0100

    is_huggingface_spaces() added

---
+++ src/utils/is_huggingface_spaces.py
@@ -0,0 +1,14 @@
+"""
+PROMPT> IS_HUGGINGFACE_SPACES=true python -m src.utils.is_huggingface_spaces
+True
+
+PROMPT> python -m src.utils.is_huggingface_spaces 
+False
+"""
+import os
+
+def is_huggingface_spaces() -> bool:
+    return os.getenv("IS_HUGGINGFACE_SPACES", "false").lower() == "true"
+
+if __name__ == "__main__":
+    print(is_huggingface_spaces())
