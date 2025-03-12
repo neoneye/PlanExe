96b30acfd7cd1331bf0de84729bf382cbc1e5560
Simon Strandgaard <neoneye@gmail.com>
2025-03-12 19:22:47 +0100

    append to the log file, instead of overwrite

--- src/plan/run_plan_pipeline.py
+++ src/plan/run_plan_pipeline.py
@@ -2008,7 +2008,7 @@ if __name__ == '__main__':
     # Capture logs messages to 'run/yyyymmdd_hhmmss/log.txt'
     file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     log_file = os.path.join(run_dir, "log.txt")
-    file_handler = logging.FileHandler(log_file, mode='w')
+    file_handler = logging.FileHandler(log_file, mode='a')
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(file_formatter)
     logger.addHandler(file_handler)
