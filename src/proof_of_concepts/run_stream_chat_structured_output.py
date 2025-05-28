cef7e6afa47b5e17a99925371effa4543f81ae6c
Simon Strandgaard <neoneye@gmail.com>
2025-05-28 13:49:12 +0200

    Extracted custom callback handlers to separate file

--- src/proof_of_concepts/run_stream_chat_structured_output.py
+++ src/proof_of_concepts/run_stream_chat_structured_output.py
@@ -3,9 +3,6 @@ from dataclasses import dataclass, field
 from src.llm_factory import get_llm
 from pydantic import BaseModel, Field
 from llama_index.core.llms import ChatMessage, MessageRole
-from llama_index.core.callbacks import CallbackManager, TokenCountingHandler
-from llama_index.core.callbacks.schema import CBEventType, EventPayload
-from llama_index.core.callbacks.base_handler import BaseCallbackHandler
 from llama_index.core.instrumentation import get_dispatcher
 from llama_index.core.instrumentation.event_handlers.base import BaseEventHandler
 from llama_index.core.instrumentation.events.llm import LLMChatInProgressEvent
@@ -67,43 +64,6 @@ class ChatProgressPrinter(BaseEventHandler):
             print(f"Acc : {event.response.message.content!r}")
             print(f"Tags : {event.tags!r}")
 
-class MyHandler(BaseCallbackHandler):
-
-    def start_trace(self, trace_id: Optional[str] = None) -> None:
-        print("start_trace")
-        return
-
-    def end_trace(
-        self,
-        trace_id: Optional[str] = None,
-        trace_map: Optional[Dict[str, List[str]]] = None,
-    ) -> None:
-        print("end_trace")
-        return
-
-    def on_event_start(
-        self,
-        event_type: CBEventType,
-        payload: Optional[Dict[str, Any]] = None,
-        event_id: str = "",
-        parent_id: str = "",
-        **kwargs: Any,
-    ) -> str:
-        print(f"on_event_start event_type: {event_type}")
-        return event_id
-
-    def on_event_end(
-        self,
-        event_type: CBEventType,
-        payload: Optional[Dict[str, Any]] = None,
-        event_id: str = "",
-        **kwargs: Any,
-    ) -> None:
-        print(f"on_event_end event_type: {event_type}")
-        print(f"payload: {payload}")
-        print(f"event_id: {event_id}")
-        print(f"kwargs: {kwargs}")
-
 
 class CostType(str, Enum):
     cheap = 'cheap'
@@ -140,31 +100,18 @@ messages = [
         content="I want to visit to Mars."
     ),
 ]
-token_counter = TokenCountingHandler(verbose=True)
-my_handler = MyHandler(event_starts_to_ignore=[], event_ends_to_ignore=[])
-sllm = llm.as_structured_llm(
-    ExtractDetails,
-    callback_manager=CallbackManager([token_counter, my_handler])
-)
+sllm = llm.as_structured_llm(ExtractDetails)
 
-count = 0
 with instrument_tags({"tag1": "tag1"}):
+    index = 0
     for chunk in sllm.stream_chat(messages):
-        print(f"chunk: {chunk}")
+        print(f"\nindex: {index} chunk: {chunk}")
         if chunk.raw:
             print(f"type of raw: {type(chunk.raw)}")
             print("raw: ", chunk.raw)
             print("Partial object:", chunk.raw.model_dump())
 
-        count += 1
-        if count % 10 == 0:
-            print(f"count: {count}  total_llm_token_count: {token_counter.total_llm_token_count}")
+        index += 1
 
 print(f"\n\nintercepted_response.message_old\n{intercepted_response.message_old}")
 print(f"\n\nintercepted_response.message_new\n{intercepted_response.message_new}\n")
-
-print("Token counts:")
-print(f"total_llm_token_count: {token_counter.total_llm_token_count}")
-print(f"prompt_llm_token_count: {token_counter.prompt_llm_token_count}")
-print(f"completion_llm_token_count: {token_counter.completion_llm_token_count}")
-print(f"total_embedding_token_count: {token_counter.total_embedding_token_count}")
