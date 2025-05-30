dcea9ff69f5236e60fae35f23ebb9ff095d3a704
Simon Strandgaard <neoneye@gmail.com>
2025-05-30 23:39:12 +0200

    Printing out more debug info about what is going wrong

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,33 @@
 import sys
+import os
+import logging
 
-project_home = '/home/neoneye/git/PlanExe'
+# Configure logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+# Get the absolute path of the current directory
+project_home = os.path.dirname(os.path.abspath(__file__))
+logger.debug(f"Project home: {project_home}")
+
 if project_home not in sys.path:
     sys.path = [project_home] + sys.path
+    logger.debug(f"Updated sys.path: {sys.path}")
 
 # Import the Flask app
+logger.debug("Attempting to import MyFlaskApp...")
 from src.ui_flask.app import MyFlaskApp
+logger.debug("Successfully imported MyFlaskApp")
 
 # Create the application instance
+logger.debug("Creating MyFlaskApp instance...")
 app = MyFlaskApp()
+logger.debug("Successfully created MyFlaskApp instance")
+
+# Enable debug mode
+app.app.debug = True
+logger.debug("Debug mode enabled")
 
 # This is the variable that PythonAnywhere will look for
-application = app 
+application = app
+logger.debug("WSGI application configured") 
