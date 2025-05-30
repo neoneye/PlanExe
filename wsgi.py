a1dde44b6bada221c9e9c498d518c7b8e0ed5c26
Simon Strandgaard <neoneye@gmail.com>
2025-05-30 23:23:28 +0200

    wsgi experiments to get the hello world to work

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,10 @@
 import sys
-import os
 
-# Add the project root directory to Python path
-sys.path.insert(0, os.path.dirname(__file__))
+project_home = '/home/neoneye/git/PlanExe'
+if project_home not in sys.path:
+    sys.path = [project_home] + sys.path
 
-# Import the Flask app
-from src.ui_flask.app import MyFlaskApp
-
-# Create the application instance
-app = MyFlaskApp().app
+from src.ui_flask.app2 import app
 
 # This is the variable that PythonAnywhere will look for
 application = app 
