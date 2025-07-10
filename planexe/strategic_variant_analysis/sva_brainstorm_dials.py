278d87b228fffb2a4b8c34ee045c446b2478aae9
Simon Strandgaard <neoneye@gmail.com>
2025-07-10 11:55:02 +0200

    Moved to separate dir

--- planexe/strategic_variant_analysis/sva_brainstorm_dials.py
+++ planexe/strategic_variant_analysis/sva_brainstorm_dials.py
@@ -13,7 +13,7 @@ Step 3:
 - With all the permutations of the dials and their values, take 20 random samples.
 - 80/20 rule: Identify the most significant 4 samples. Discard the rest.
 
-PROMPT> python -m planexe.plan.strategic_variant_analysis8020
+PROMPT> python -m planexe.strategic_variant_analysis.sva_brainstorm_dials
 """
 import json
 import time
